0cb48a50e1d88baa3863797ebca04783dd981aeb
louisomo <louisliberty65@gmail.com>
2022-09-08 06:43:18 +0100

    updated index.html style.css  and script.js

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ let DaysOfWeek = [
 let dateToday = new Date();
 let dayToday = dateToday.getDay();
 dayRef.innerHTML = `Today is <span>${DaysOfWeek[dayToday]}</span>`;
+
+let container = document.querySelector(".container");
+let timeNow = new Date().getHours();
+let greeting =
+  timeNow >= 00 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 16
+    ? "Good Afternoon"
+    : "Good evening";
+container.innerHTML = `<h1>${greeting}</h1>`;
